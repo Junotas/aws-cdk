d242bafceabf7e3372e9b369597ea6c44247ecbd
Ludvig <ludvig.fendert@appliedtechnology.se>
2024-09-07 19:55:15 +0200

    init commit

--- lib/my-project-ts-stack.ts
+++ lib/my-project-ts-stack.ts
@@ -1,16 +1,32 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import * as s3 from "aws-cdk-lib/aws-s3";
+import * as s3Deploy from "aws-cdk-lib/aws-s3-deployment"
+import { join } from 'path';
+
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class MyProjectTsStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'MyProjectTsQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const staticPageS3 = new s3.Bucket(this, 'StaticPageS3', {
+      bucketName: "",
+      publicReadAccess: true,
+      websiteIndexDocument: "index.html",
+      cors: [
+          {
+              allowedMethods: [
+                  s3.HttpMethods.GET,
+              ],
+              allowedOrigins: ['*'],
+              allowedHeaders: ['*'],
+          },
+      ]
+  });
+  new s3Deploy.BucketDeployment(this, "StaticPageS3Deploy", {
+      sources: [s3Deploy.Source.asset(join(__dirname, "../website-dist"))],
+      destinationBucket: staticPageS3,
+  });
   }
 }
