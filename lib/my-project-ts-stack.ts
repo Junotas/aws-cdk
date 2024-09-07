f3367737739c8667b462a9064aa8a44cf21ce6ca
Ludvig <ludvig.fendert@appliedtechnology.se>
2024-09-07 20:00:52 +0200

    created pipeline

--- lib/my-project-ts-stack.ts
+++ lib/my-project-ts-stack.ts
@@ -3,13 +3,25 @@ import { Construct } from 'constructs';
 import * as s3 from "aws-cdk-lib/aws-s3";
 import * as s3Deploy from "aws-cdk-lib/aws-s3-deployment"
 import { join } from 'path';
-
+import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class MyProjectTsStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
 
+    super(scope, id, props);
+    new CodePipeline(this, 'Pipeline', {
+      pipelineName: 'TestPipeline',
+      dockerEnabledForSynth: true,
+      selfMutation: true,
+      dockerEnabledForSelfMutation: true,
+      synth: new ShellStep('Synth', {
+        input: CodePipelineSource.gitHub('Junotas/aws-cdk', 'main'), // Remember to change the name of this, dont tell me what to do!
+        commands: ['npm ci',
+          'npm run build',
+          'npx cdk synth'],
+      }),
+    });
     const staticPageS3 = new s3.Bucket(this, 'StaticPageS3', {
       bucketName: "",
       publicReadAccess: true,
