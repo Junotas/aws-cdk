5e3dacfe295795a18fda7f11cc666b5faaeb32a5
Ludvig <ludvig.fendert@appliedtechnology.se>
2024-09-07 20:40:00 +0200

    added html and deployed the site

--- lib/my-project-ts-stack.ts
+++ lib/my-project-ts-stack.ts
@@ -1,44 +1,52 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as s3 from "aws-cdk-lib/aws-s3";
-import * as s3Deploy from "aws-cdk-lib/aws-s3-deployment"
+import * as s3Deploy from "aws-cdk-lib/aws-s3-deployment";
 import { join } from 'path';
 import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class MyProjectTsStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
 
     super(scope, id, props);
+
     new CodePipeline(this, 'Pipeline', {
       pipelineName: 'TestPipeline',
       dockerEnabledForSynth: true,
       selfMutation: true,
       dockerEnabledForSelfMutation: true,
       synth: new ShellStep('Synth', {
-        input: CodePipelineSource.gitHub('Junotas/aws-cdk', 'main'), // Remember to change the name of this, dont tell me what to do!
-        commands: ['npm ci',
+        input: CodePipelineSource.gitHub('Junotas/aws-cdk', 'main', {
+          authentication: cdk.SecretValue.secretsManager('github-oauth-token')
+        }),
+        commands: [
+          'npm ci',
           'npm run build',
-          'npx cdk synth'],
+          'npx cdk synth'
+        ],
       }),
     });
+
     const staticPageS3 = new s3.Bucket(this, 'StaticPageS3', {
-      bucketName: "",
+      bucketName: "nyek-buzow-asdfghjkloissujnmbsaas",
       publicReadAccess: true,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ACLS, // Allow bucket-level public access but block ACL-based public access
       websiteIndexDocument: "index.html",
       cors: [
-          {
-              allowedMethods: [
-                  s3.HttpMethods.GET,
-              ],
-              allowedOrigins: ['*'],
-              allowedHeaders: ['*'],
-          },
-      ]
-  });
-  new s3Deploy.BucketDeployment(this, "StaticPageS3Deploy", {
+        {
+          allowedMethods: [
+            s3.HttpMethods.GET,
+          ],
+          allowedOrigins: ['*'],
+          allowedHeaders: ['*'],
+        },
+      ],
+    });
+
+    new s3Deploy.BucketDeployment(this, "StaticPageS3Deploy", {
       sources: [s3Deploy.Source.asset(join(__dirname, "../website-dist"))],
       destinationBucket: staticPageS3,
-  });
+    });
+
   }
 }
