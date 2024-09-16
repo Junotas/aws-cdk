cbcedeb0bf3bbe3e5808a64c74af797467266905
Ludvig <ludvig.fendert@appliedtechnology.se>
2024-09-16 13:56:43 +0200

    refactored for cloudfront

--- lib/my-project-ts-stack.ts
+++ lib/my-project-ts-stack.ts
@@ -1,70 +1,66 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-import * as s3 from "aws-cdk-lib/aws-s3";
-import * as s3Deploy from "aws-cdk-lib/aws-s3-deployment";
-import * as iam from 'aws-cdk-lib/aws-iam';
-import { join } from 'path';
-import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
+import {
+  aws_s3 as s3,
+  aws_cloudfront as cloudfront,
+  aws_s3_deployment as s3deploy,
+  aws_cloudfront_origins as origins,
+  RemovalPolicy,
+} from 'aws-cdk-lib';
 
- // Log the absolute path of website-dist for debugging
- console.log("Asset path:", join(__dirname, "../website-dist"));
-
-export class MyProjectTsStack extends cdk.Stack {
+export class MyStaticWebsiteStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-
     super(scope, id, props);
 
-   
-
-    new CodePipeline(this, 'Pipeline', {
-      pipelineName: 'TestPipeline',
-      dockerEnabledForSynth: true,
-      selfMutation: true,
-      dockerEnabledForSelfMutation: true,
-      synth: new ShellStep('Synth', {
-        input: CodePipelineSource.gitHub('Junotas/aws-cdk', 'main', {
-          authentication: cdk.SecretValue.secretsManager('github-oauth-token')
-        }),
-        commands: [
-          'npm ci',
-          'npm run build',
-          'npx cdk synth'
-        ],
-      }),
+    // S3 Bucket to store website files
+    const websiteBucket = new s3.Bucket(this, 'WebsiteBucket', {
+      bucketName: 'nyek-buzow-asdfghjkloissujnmbsaas',
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+      // Optional: Automatically delete bucket when the stack is destroyed
+      removalPolicy: RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
     });
 
-    const staticPageS3 = new s3.Bucket(this, 'StaticPageS3', {
-      bucketName: "nyek-buzow-asdfghjkloissujnmbsaas",
-      objectOwnership: s3.ObjectOwnership.BUCKET_OWNER_ENFORCED, // Enforce bucket owner
-      blockPublicAccess: new s3.BlockPublicAccess({
-        blockPublicAcls: false,
-        ignorePublicAcls: false,
-        blockPublicPolicy: false,
-        restrictPublicBuckets: false
-      }),
-      websiteIndexDocument: "index.html",
-      cors: [
+    // Origin Access Identity for CloudFront to access S3
+    const originAccessIdentity = new cloudfront.OriginAccessIdentity(this, 'OAI');
+
+    // Grant read permissions to CloudFront OAI
+    websiteBucket.grantRead(originAccessIdentity);
+
+    // CloudFront distribution
+    const distribution = new cloudfront.Distribution(this, 'WebsiteDistribution', {
+      defaultRootObject: 'index.html',
+      defaultBehavior: {
+        origin: new origins.S3Origin(websiteBucket, { originAccessIdentity }),
+        viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+      },
+      errorResponses: [
         {
-          allowedMethods: [
-            s3.HttpMethods.GET,
-          ],
-          allowedOrigins: ['*'],
-          allowedHeaders: ['*'],
+          httpStatus: 403,
+          responseHttpStatus: 200,
+          responsePagePath: '/index.html',
+          ttl: cdk.Duration.minutes(5),
+        },
+        {
+          httpStatus: 404,
+          responseHttpStatus: 200,
+          responsePagePath: '/index.html',
+          ttl: cdk.Duration.minutes(5),
         },
       ],
     });
 
-    // Add a bucket policy to allow public read access
-    staticPageS3.addToResourcePolicy(new iam.PolicyStatement({
-      actions: ['s3:GetObject'],
-      resources: [staticPageS3.arnForObjects('*')],
-      principals: [new iam.AnyPrincipal()],
-    }));
-
-    new s3Deploy.BucketDeployment(this, "StaticPageS3Deploy", {
-      sources: [s3Deploy.Source.asset(join(__dirname, "../website-dist"))],
-      destinationBucket: staticPageS3,
+    // Deploy website contents to S3 bucket
+    new s3deploy.BucketDeployment(this, 'DeployWebsite', {
+      sources: [s3deploy.Source.asset('./website-dist')],
+      destinationBucket: websiteBucket,
+      distribution,
+      distributionPaths: ['/*'], // Invalidate cache after deployment
     });
 
+    // Output the CloudFront URL
+    new cdk.CfnOutput(this, 'CloudFrontURL', {
+      value: distribution.distributionDomainName,
+    });
   }
 }
